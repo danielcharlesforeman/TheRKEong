d59125e554429bcdd92e5b2d04f22e55dbad9b7c
danielcharlesforeman <70060160+danielcharlesforeman@users.noreply.github.com>
2021-01-02 05:06:29 +0000

    Changed to physics engine
    Changed from direct logic and math to using the Arcade physics engine.

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
 var gameScene = new Phaser.Scene('game');
-var score = { id: null, value: 0 };
+var score = {
+    id: null,
+    value: 0
+};
 var lives = 3;
-var player, ball, ai, music, padSND, bounceSND, round, rounds, background, emitter, particles;
+var player, ball, ai, music, padSND, bounceSND, round, rounds, background, emitter, particles, leftWall, rightWall;
 var gamemode = false;
 
 var config = {
@@ -9,42 +12,56 @@ var config = {
     width: 2000,
     height: 1125,
     scene: gameScene,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 0
+            },
+            debug: true
+        }
+    },
 }
 
 var game = new Phaser.Game(config);
 
-gameScene.init = function() {
-    
+gameScene.init = function () {
+
 }
 
-gameScene.preload = function() {
+gameScene.preload = function () {
     this.load.image('backdrop', './Assets/PNG/Retina/background_brown.png');
     this.load.image('player', './Assets/PNG/Retina/block_narrow.png');
     this.load.image('ai', './Assets/PNG/Retina/block_narrow2.png');
     this.load.image('ball', './Assets/PNG/Retina/ball_red_small.png');
     this.load.image('star', './Assets/PNG/Retina/star.png');
-    
+    this.load.image('wall', './Assets/PNG/Retina/wall.png');
+
     this.load.audio('newer', './newer-wave-by-kevin-macleod-from-filmmusic-io.mp3');
     this.load.audio('bounce', './bounce.wav');
     this.load.audio('pad', './pad.wav');
 }
 
-gameScene.create = function() {
+gameScene.create = function () {
 
-    background = this.add.tileSprite( 0, 0, 2000, 1125, 'backdrop');
-    background.setPosition( 1000, 1125 / 2)
-    
+    background = this.add.tileSprite(0, 0, 2000, 1125, 'backdrop');
+    background.setPosition(1000, 1125 / 2)
+
     music = this.sound.add('newer');
     padSND = this.sound.add('pad');
     bounceSND = this.sound.add('bounce');
 
     this.cursorKeys = this.input.keyboard.createCursorKeys();
-    
-    player = this.add.sprite( 0, 0, 'player' );
-    player.setOrigin( 0.5 );
-    player.x = 100;
-    player.y = 1125/2;
-    player.score = this.add.text( 1000 - ( 32 * 2 ), 50, '0', { fontFamily: 'monospace', fontSize: '32px', fill: '#0FF' } );
+
+    player = this.physics.add.sprite(50, 1125 / 2, 'player');
+    player.setOrigin(0.5);
+    //this.physics.add.existing(player);
+    player.body.immovable = true;
+    player.score = this.add.text(1000 - (32 * 2), 50, '0', {
+        fontFamily: 'monospace',
+        fontSize: '32px',
+        fill: '#0FF'
+    });
     player.value = 0;
 
     particles = this.add.particles('star');
@@ -53,69 +70,89 @@ gameScene.create = function() {
     emitter.setBlendMode(Phaser.BlendModes.ADD);
     emitter.minParticleScale = 0.15;
     emitter.maxParticleScale = 0.35;
-    
-    ball = this.add.sprite( 0, 0, 'ball' );
-    ball.setOrigin( 0.5 );
+
+    ball = this.physics.add.sprite(0, 0, 'ball');
     ball.x = 1000;
-    ball.y = 1125/2;
-    ball.sx = 4 + ( Math.random() * 2 );
-    ball.sy = ( (4 + ( Math.random() * 2 ) * 0.5265 )  );
-    ball.accel = 0.75;
-    if ( Math.round( Math.random() ) == 0 ) { ball.flagX = true; } else { ball.flagX = false; }
-    if ( Math.round( Math.random() ) == 0 ) { ball.flagY = true; } else { ball.flagY = false; }
+    ball.y = 550;
+    ball.setOrigin(0.5);
+    ball.setVelocity( randomDirection(350), randomDirection(350) );
+    ball.setBounce(1.01, 1.01);
+    ball.setCollideWorldBounds(true);
 
-    ai = this.add.sprite( 0, 0, 'ai' );
-    ai.tint = 0xff0000;
-    ai.setOrigin( 0.5 );
-    ai.x = 1900;
-    ai.y = 1125/2;
-    ai.score = this.add.text( 1000 + ( 32 * 2 ), 50, '0', { fontFamily: 'monospace', fontSize: '32px', fill: '#F00' } );
+    ai = this.physics.add.sprite(1950, 1125 / 2, 'ai');
+    ai.setOrigin(0.5);
+    //this.physics.add.existing(ai);
+    ai.body.immovable = true;
+    ai.score = this.add.text(1000 + (32 * 2), 50, '0', {
+        fontFamily: 'monospace',
+        fontSize: '32px',
+        fill: '#F00'
+    });
     ai.value = 0;
-    
-    round = this.add.text ( 900, 1000, 'Round 0', { fontFamily: 'monospace', fontSize: '32px', fill: '#FFF' } )
 
-    this.add.text ( 1000, 50, '|', { fontFamily: 'monospace', fontSize: '32px', fill: '#FFF' } )
+    rightWall = this.physics.add.sprite(1990, 1125/2, 'wall');
+    leftWall = this.physics.add.sprite(10, 1125/2, 'wall');
+    
+    rightWall.body.immovable = true;
+    leftWall.body.immovable = true;
+
+    round = this.add.text(900, 1000, 'Round 0', {
+        fontFamily: 'monospace',
+        fontSize: '32px',
+        fill: '#FFF'
+    })
+
+    this.add.text(1000, 50, '|', {
+        fontFamily: 'monospace',
+        fontSize: '32px',
+        fill: '#FFF'
+    })
 
 }
 
-gameScene.update = function() {
+gameScene.update = function () {
 
-    if ( gamemode == true ) {
+    if (gamemode == true) {
 
-        if ( this.cursorKeys.up.isDown ) {
-            player.y-=5;
+        if (this.cursorKeys.up.isDown) {
+            player.y -= 7;
         }
 
-        if ( this.cursorKeys.down.isDown ) {
-            player.y+=5;
+        if (this.cursorKeys.down.isDown) {
+            player.y += 7;
         }
 
-        if ( player.y > 1125 ) { player.y = 1125 }
-        if ( player.y < 0 ) { player.y = 0 }
-    
-        if ( ball.flagX == true ) { ball.x+=ball.sx };
-        if ( ball.flagX == false ) { ball.x-=ball.sx };
+        if (ball.body.velocity.x > 0 && Phaser.Math.Distance.BetweenPoints(ball, ai) < 1500) {
+            if (ai.y > ball.y) {
+                ai.y -= 7
+            }
+            if (ai.y < ball.y) {
+                ai.y += 7
+            }
+        }
 
-        if ( ball.flagY == true ) { ball.y+=ball.sy };
-        if ( ball.flagY == false ) { ball.y-=ball.sy };
+        this.physics.world.collide(ball, [player, ai, leftWall, rightWall]);
 
-        if ( ball.flagX == true && ball.x > 2000 ) { ball.flagX = false; ball.sx += ball.accel; player.value++; player.score.setText( player.value ); bounceSND.play(); reset();  };
-        if ( ball.flagX == false && ball.x < 0 ) { ball.flagX = true; ball.sx += ball.accel; ai.value++; ai.score.setText( ai.value ); bounceSND.play(); reset() };
 
-        if ( ball.flagY == true && ball.y > 1125 ) { ball.flagY = false; ball.sy += ball.accel * 0.5265; bounceSND.play(); };
-        if ( ball.flagY == false && ball.y < 0 ) { ball.flagY = true; ball.sy += ball.accel * 0.5265; bounceSND.play(); };
+        if (ball.body.blocked.up || ball.body.blocked.down || ball.body.blocked.left || ball.body.blocked.right) {
+            bounceSND.play()
+        };
 
-        if ( ball.flagX == false && ball.y < player.y + 125 && ball.y > player.y - 125 && ball.x < player.x + 25 && ball.x > player.x - 25 ) { ball.flagX = true; ball.sx += ball.accel; padSND.play(); }
-        if ( ball.flagX == true && ball.y < ai.y + 125 && ball.y > ai.y - 125 && ball.x > ai.x - 25 && ball.x < ai.x + 25 ) { ball.flagX = false; ball.sx += ball.accel; padSND.play(); }
-
-        if ( ball.flagX == true && ball.x > ai.level ) { 
-            if ( ball.y > ai.y ) { ai.y+=5 }
-            if ( ball.y < ai.y ) { ai.y-=5 }
+        if ( Phaser.Geom.Intersects.RectangleToRectangle( ball.getBounds(), rightWall.getBounds() ) ) {
+            player.value++;
+            player.score.setText(player.value);
+            reset();
         }
         
-        emitter.setPosition( ball.x, ball.y );
+        if ( Phaser.Geom.Intersects.RectangleToRectangle( ball.getBounds(), leftWall.getBounds() ) ) {
+            ai.value++;
+            ai.score.setText(ai.value);
+            reset();
+        }
 
     }
+
+    emitter.setPosition(ball.x, ball.y);
 
 }
 
@@ -124,26 +161,38 @@ function start() {
     document.getElementById("level").style.display = 'none';
     let tmp = document.getElementById("ai").value;
     rounds = document.getElementById("rounds").value;
-    ai.level = 2000 * ( 1 - tmp );
+    ai.level = 2000 * (1 - tmp);
     music.play();
-    round.setText('rounds left ' + rounds )
+    round.setText('rounds left ' + rounds);
+    ball.x = 1000;
+    ball.y = 1125 / 2;
+    ball.setVelocity( Math.random()*700-350, Math.random()*700-350 );
 }
 
 function reset() {
     ball.x = 1000;
-    ball.y = 1125/2;
-    ball.sx = 4 + ( Math.random() * 2 );
-    ball.sy = ( (4 + ( Math.random() * 2 ) * 0.5265 )  );
+    ball.y = 1125 / 2;
+    ball.setVelocity( randomDirection(350), randomDirection(350) );
     rounds--;
-    round.setText('rounds left ' + rounds )
-    if ( rounds <= 0 ) {
-        if ( ai.value > player.value ) { alert('Computer has won!') };
-        if ( ai.value < player.value ) { alert('Player has won!')};
-        if ( ai.value == player.value ) { alert('Match ends in draw!')};
-         document.getElementById("level").style.display = 'block';
+    round.setText('rounds left ' + rounds)
+    if (rounds <= 0) {
+        if (ai.value > player.value) {
+            alert('Computer has won!')
+        };
+        if (ai.value < player.value) {
+            alert('Player has won!')
+        };
+        if (ai.value == player.value) {
+            alert('Match ends in draw!')
+        };
+        document.getElementById("level").style.display = 'block';
         gamemode = false;
         music.stop();
         ai.value = 0;
         player.value = 0;
     }
+}
+
+function randomDirection(n) {
+    if( Math.round(Math.random()) == 0 ) { return n; } else { let tmp2=0-n; return tmp2; };
 }
